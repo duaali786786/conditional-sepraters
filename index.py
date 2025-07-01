467ca508a49925c25ed60ea3f2a7f509e024c39c
duaali786786 <duanaval64@gmail.com>
2025-07-01 21:24:29 +0700

    j

---
+++ index.py
@@ -0,0 +1,9 @@
+num=int(input("Enter your number"))
+
+if num>0:
+    print(f"{num} is a positive number")
+elif num==0:
+    print(f"{num} is a neither a positive or negative number")
+else:
+    print(f"{num} is a negative number")
+    
